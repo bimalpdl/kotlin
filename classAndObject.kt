5064ac750fca4895da47b8b9762a56cb52a9d162
Bimal <me.bimalpdl1@gmail.com>
2024-08-05 01:09:44 +0545

    Added class with constructor file

--- classAndObject.kt
+++ classAndObject.kt
@@ -1,43 +1,30 @@
-// class and objects in kotlin
+import kotlin.math.sin
+
 fun main() {
-    print("Enter number of entries you wanna make: ")
-    val numOfEntries: Int= readln().toInt()
-    for (i in 0 until numOfEntries) {
-        println("Enter your name: ")
-        val name: String = readln()
-        println("Enter your age: ")
-        val age: Int = readln().toInt()
-        println("Are you an student? ( yes | no )")
-        val choice: String = readln()
-        val isStudent: Boolean = if(choice == "yes") true else false
-        println("You are student? $isStudent")
+    print("Enter car brand name: ")
+    val name: String = readln()
+    println()
 
-        println()
-       val person =  Person(name,age,isStudent)
-        println(person.name)    // accessing class properties
-        person.greet()    // accessing class methods
-    }
+    print("Enter car model: ")
+    val model: String = readln()
+    println()
+    print("Enter total distance covered : ")
+    val distanceCovered: Double = readln().toDouble()
 
+    val car1 = Cars()
+    car1.name = name
+    car1.model = model
+    car1.distanceCovered = distanceCovered
+    val retunedValue  = car1.aboutCar()
+    println(retunedValue)
 }
- // defining class is similar to defining a function; 'class' keyword is used
-class Person(val name: String, val age: Int, val isStudent: Boolean ) {    // class properties
-    fun greet() {    // class method
-        println("Hello, $name ! How are you doing?")
-        val response: String = readln()
-        when(response) {
-            "Good" -> println("Glad to hear that. Have a good day!")
-            "Bad" -> println("You'll be alright. Have a good day! ")
-            else -> println("You might have entered wrong choice, please enter good or bad.")
-        }
-        println("So you're $age years old huh?\n")
-        if(isStudent){
-            println("Best of luck for the next exam pal.")
-        }
-        else{
-            println("Have a good luck finding a job.")
-        }
-
+// class declaration without constructor
+class Cars{    // no parameter passed/assigned
+    var name: String = ""
+    var model: String = ""
+    var distanceCovered: Double = 0.0
+    fun aboutCar(): String {
+        return "The car name is $name\nModel number = $model\nTotal distance covered = $distanceCovered"
 
     }
-
 }
