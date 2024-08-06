6dfe6e396b448a6fcdc1da3d4def337e2f3015f2
Bimal <me.bimalpdl1@gmail.com>
2024-08-07 01:51:22 +0545

    added list.kt

--- hello.kt
+++ hello.kt
@@ -1,4 +1,3 @@
 fun main ()
 {
- println("Hello world, This is my first project on kotlin")
 }
---
+++ input.kt
@@ -0,0 +1,6 @@
+fun main (){
+    val name : String
+    println("Enter your name")
+    name = readLine()
+    println("Your name is $name")
+}
